263493853c9de4eff6189d40c0ac0ed8283c7b40
Ethan <ethaneye02@gmail.com>
2026-06-10 04:26:39 -0600

    shared mem setup

--- homework_3_extracredit/Checker.c
+++ homework_3_extracredit/Checker.c
@@ -4,9 +4,25 @@
 #include <stdlib.h>
 #include <sys/resource.h>
 #include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 int main(int argc, char *argv[])
 {
+    // pipe stuff extract shared mem id ...
+    // process receive the read end of the pipe
+    // put the reference in a local variable 
+    int sharedMemId = 0;
+    // get a pointer to shared memory and attach to the segment
+    int *sharedMemPointer = (int *)shmat(sharedMemId, NULL, 0);
+    // write to shared memory
+    // increment mem pointer to add more data 
+    // + 1 moves towards higher address -1 moves towards lower address 4 bytes
+    
+    // do some math and write either a 1 or a 0 to shared memory segment
+    *sharedMemPointer = 1;
+    //detatch
+    //schmdt(sharedMemPointer);
 
     printf("Checker process [%d]: Starting.\n", getpid());
 
--- homework_3_extracredit/Coordinator.c
+++ homework_3_extracredit/Coordinator.c
@@ -1,9 +1,11 @@
-#include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/resource.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 // Ethan Eye Homework 2
 // pid_t is a data type for process IDs it is the same as a int just for readability meaning (process ID type).
@@ -17,6 +19,12 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // create shared memory segment (its seperate from all processes)
+    int sharedMemId = shmget(IPC_PRIVATE,sizeof(int),IPC_CREAT | 0666 );
+    
+    // parent needs to write shared memory to a pipe
+    // and pass a reference to the read end of the pipe and then call execlp
+
     for (int i = 2; i < argc; i++)
     {
         if (atoi(argv[i]) < 0)
@@ -24,26 +32,39 @@ int main(int argc, char *argv[])
             printf("Coordinator: invalid arguments\n");
             return 1;
         }
-
-        // create a child process
+        
+        
+        // fork spawns new child process
         pid_t pid = fork();
+       
 
         // run only for child processes
         if (pid == 0)
         {
+            // get shared memory to child somehow 
+            // using pipes
+            // using execlp
             execlp("./checker", "checker", argv[1], argv[i], NULL);
             exit(0);
         }
-        else
+        // for parent process
+        else 
         {
+            
             printf("Coordinator: forked process with ID %d.\n", pid);
             printf("Coordinator: waiting for process [%d].\n", pid);
             int status;
+            // still wait for child to finish
+
             wait(&status);
+            // parent should attach to shared memory segment after the children finish 
+            // then parent should read from shared memory segment
             printf("Coordinator: child process %d returned %d.\n", pid, WEXITSTATUS(status));
           
         }
     }
+    // destroy shared memory segment
+    // shmctl(*sharedMemPointer, IPC_RMID, NULL);
     printf("Coordinator: exiting.\n");
     return 0;
 }
